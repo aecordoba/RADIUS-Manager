ba6f10f8202162348801fc12720fe26ac26f4fba
Adrián E. Córdoba <software.asia@gmail.com>
2022-11-09 15:18:25 -0300

    Test users creation.

--- System/DB/scripts/data.sql
+++ System/DB/scripts/data.sql
@@ -2,9 +2,9 @@
 -- drop User 'rmapp'@'%';
 -- flush privileges;
 
-CREATE User 'radman'@'%' IDENTIFIED BY 'radman123';
+-- CREATE User 'radman'@'%' IDENTIFIED BY 'radman123';
 GRANT ALL PRIVILEGES ON `RadiusManager`.* TO 'radman'@'%';
-CREATE User 'rmapp'@'%' IDENTIFIED BY 'rmapp123';
+-- CREATE User 'rmapp'@'%' IDENTIFIED BY 'rmapp123';
 GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE, SHOW VIEW ON `RadiusManager`.* TO 'rmapp'@'%';
 GRANT SELECT ON mysql.proc TO 'rmapp'@'%';
 
@@ -12,3 +12,14 @@ flush privileges;
 
 use RadiusManager;
  
+INSERT INTO `RadiusManager`.`Authorities` VALUES (1,'ADMIN'),
+                                (2,'USER'),
+                                (3,'OBSERVER');
+
+INSERT INTO Users (id,name,password,enabled,first_name,middle_name,last_name) VALUES (1,'admin','$2a$10$kEGwwhWZpV2ApEkEsTXUROFKV4vhkKxaMlZCl7XQ5IfG5Z1GFGVcC',1,'Super','','Administrator');
+INSERT INTO Users (id,name,password,enabled,first_name,middle_name,last_name) VALUES (2,'user','$2a$10$1lCpQKdv7FQ8ieFJwAVu0uv.x6nqhnEl3mfFJiFyLG36wBnPFtp4W',1,'User','','User');
+INSERT INTO Users (id,name,password,enabled,first_name,middle_name,last_name) VALUES (3,'obs','$2a$10$VR1N3m6r5POy3NZtr056y.y2axn1gmPWzfguHpO3NHKYkqnBY3Irq',1,'Observer','','Observer');
+
+INSERT INTO Users_Authorities(user, authority) VALUES(1, 1);
+INSERT INTO Users_Authorities(user, authority) VALUES(2, 2);
+INSERT INTO Users_Authorities(user, authority) VALUES(3, 3);
